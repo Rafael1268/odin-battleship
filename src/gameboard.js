6b6688fe55d72ce6dd70cd278ab425508b182d56
RafaelB <rafaelboschmamail@gmail.com>
2022-06-02 14:08:09 +0200

    Place function now creates ship and puts it in ships array

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,5 @@
+import Ship from './ship';
+
 class GameBoard {
   constructor() {
     this.board = [
@@ -12,6 +14,7 @@ class GameBoard {
       ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
       ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
     ];
+    this.ships = [];
   }
 
   place(coord1, coord2) {
@@ -20,6 +23,11 @@ class GameBoard {
     const splitCoord2 = coord2.split('');
     const letter1 = letters.indexOf(splitCoord1[0]);
     const letter2 = letters.indexOf(splitCoord2[0]);
+    if (letter2 - letter1 === 0) {
+      this.ships.push(new Ship(splitCoord2[1] - splitCoord1[1] + 1));
+    } else {
+      this.ships.push(new Ship(letter2 - letter1 + 1));
+    }
     for (let i = 0; i < letter2 - letter1 + 1; i++) {
       this.board[letter1 + i][splitCoord1[1] - 1] = 'O';
     }
