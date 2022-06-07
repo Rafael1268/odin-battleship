91429f39ea7aa9059a7bdb32ea5ccf190c95588a
RafaelB <rafaelboschmamail@gmail.com>
2022-06-07 13:57:10 +0200

    Player board rendering now works

---
+++ src/render.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable no-undef */
+import { computerBoard, playerBoard } from './index';
+
+function render() {
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+  let num1 = 0;
+  let num2 = 1;
+  playerBoard.board.forEach((row) => {
+    row.forEach((grid) => {
+      const gridS = document.getElementById(`${letters[num1]}${num2}`);
+      switch (grid) {
+        case 'E':
+          break;
+        case 'O':
+          gridS.style.backgroundColor = 'var(--attacker-l-c)';
+          break;
+        case 'X':
+          gridS.style.backgroundColor = 'var(--attacker-c';
+          break;
+        default:
+          gridS.style.backgroundColor = 'var(--defender-c';
+          break;
+      }
+      num2++;
+    });
+    num1++;
+    num2 = 1;
+  });
+}
+
+export default render;
