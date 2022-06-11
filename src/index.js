e285f2941571fb41c54904fca4e40ab3f9e3c540
RafaelB <rafaelboschmamail@gmail.com>
2022-06-11 13:14:15 +0200

    Small change

--- src/index.js
+++ src/index.js
@@ -13,29 +13,7 @@ let playerBoard;
 let player;
 let computer;
 
-function createGame() {
-  computerBoard = new GameBoard();
-  computerBoard.place('A1', 'A5');
-  computerBoard.place('B1', 'B4');
-  computerBoard.place('C1', 'C3');
-  computerBoard.place('D1', 'D3');
-  computerBoard.place('E1', 'E2');
-  playerBoard = new GameBoard();
-  playerBoard.place('A1', 'A5');
-  playerBoard.place('B1', 'B4');
-  playerBoard.place('C1', 'C3');
-  playerBoard.place('D1', 'D3');
-  playerBoard.place('E1', 'E2');
-  player = new Player('player', 0);
-  computer = new Player('computer', 1);
-}
-
-createGame();
-render();
-player.turn();
-
-const enemyGrid = document.querySelector('#enemyBoard');
-enemyGrid.addEventListener('click', () => {
+function gridClick(event) {
   if (event.target.className !== 'grid') return;
   const turn = player.checkTurn();
   if (turn === false) return;
@@ -61,6 +39,29 @@ enemyGrid.addEventListener('click', () => {
     }
     player.turn();
   }, 1100);
-});
+}
+
+function createGame() {
+  computerBoard = new GameBoard();
+  computerBoard.place('A1', 'A5');
+  computerBoard.place('B1', 'B4');
+  computerBoard.place('C1', 'C3');
+  computerBoard.place('D1', 'D3');
+  computerBoard.place('E1', 'E2');
+  playerBoard = new GameBoard();
+  playerBoard.place('A1', 'A5');
+  playerBoard.place('B1', 'B4');
+  playerBoard.place('C1', 'C3');
+  playerBoard.place('D1', 'D3');
+  playerBoard.place('E1', 'E2');
+  player = new Player('player', 0);
+  computer = new Player('computer', 1);
+  const enemyGrid = document.querySelector('#enemyBoard');
+  enemyGrid.addEventListener('click', () => gridClick(event));
+  render();
+  player.turn();
+}
+
+createGame();
 
 export { computerBoard, playerBoard, computer };
