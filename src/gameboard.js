7814215790f0fb9861fefbb395c4d214f67f8bbb
RafaelB <rafaelboschmamail@gmail.com>
2022-06-12 17:03:10 +0200

    Added check for random computer ships

--- src/gameboard.js
+++ src/gameboard.js
@@ -72,6 +72,25 @@ class GameBoard {
     return true;
   }
 
+  // Checks if space is empty
+  checkClear(coord1, coord2) {
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    const splitCoord1 = [coord1.slice(0, 1), coord1.slice(1, 3)];
+    const splitCoord2 = [coord2.slice(0, 1), coord2.slice(1, 3)];
+    const letter1 = letters.indexOf(splitCoord1[0]);
+    const letter2 = letters.indexOf(splitCoord2[0]);
+    if (splitCoord2[1] - splitCoord1[1] === 0) {
+      for (let i = 0; i < letter2 - letter1 + 1; i++) {
+        if (this.board[letter1 + i][splitCoord1[1] - 1] !== 'E') return false;
+      }
+    } else {
+      for (let i = 0; i < splitCoord2[1] - splitCoord1[1] + 1; i++) {
+        if (this.board[letter1][splitCoord1[1] - 1 + i] !== 'E') return false;
+      }
+    }
+    return true;
+  }
+
   // Clears preview from board
   clearPreview() {
     let rowNum = 0;
